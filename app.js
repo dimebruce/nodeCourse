c8f134d4f0a5c774f926f9326c2f67f57f912003
dimebruce <adriangarcia@itsx.edu.mx>
2022-08-09 10:17:24 -0500

    Recorrer un array y diferencias de count y log

---
+++ app.js
@@ -0,0 +1,8 @@
+const frutas = ['Platano 🍌', 'Manzana 🍎', 'Pera 🍐', 'Fresa 🍓']
+
+//Recorriendo el array e iterarlo 1x1
+frutas.forEach(item => {
+    // Diferencias entre count y log
+    console.log(item)
+    console.count(item)
+})
