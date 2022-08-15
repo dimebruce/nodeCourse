08b59195c8a90e87ec927485d96017d1203abc5e
dimebruce <adriangarcia@itsx.edu.mx>
2022-08-15 14:35:20 -0500

    Se creó un servidor con js puro y sando a node con el protocolo http+

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-// const frutas = ['Platano 🍌', 'Manzana 🍎', 'Pera 🍐', 'Fresa 🍓']
+// Importando el servidor http que viene por defecto por node
+const http = require('http')
 
-// Importar modulos de node cuando es solo una propiedad
-// const frutero = require('./frutero')
-
-
-// Importar cuando son dos o más propiedades -- Destructing Object
-const {frutas, dinero} = require('./frutero')
-
-// Importando el paquete cowsay instalado por npm
-const cowsay = require('cowsay')
-
-// Usando cowsay
-
-console.log(cowsay.say({
-    text : "Soy un modulo llamado cowsay",
-    e : "oO",
-    T : "U "
-}));
-
-//Recorriendo el array e iterarlo 1x1
-frutas.forEach(item => {
-    // Diferencias entre count y log
-    console.log(item)
-    console.count(item)
+// Levantando el servidor
+const server = http.createServer((req, res) => {
+    res.end('respuesta a la solicitud V.2')
 })
+// Creando el puerto 
+const port = 3000; 
+server.listen(port, () => {
 
-// Imprimiendo la seg constante que vienes desde frutas.js
-console.log(dinero)
+    console.log('escuchando solicitudes desde el puerto')
+})
